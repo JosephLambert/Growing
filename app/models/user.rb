d10f9b99e523f0f9093ff528be7616a2d174d1d3
闫磊 <leiyan@fiberead.com>
2018-11-03 09:13:25 +0800

    04 user login system

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   attr_accessor :password, :password_confirmation
 
   validates_presence_of :email, message: "邮箱不能为空"
+  validates_format_of :email, message: "邮箱格式不合法",
+    with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+    if: proc { |user| !user.email.blank? }
   validates :email, uniqueness: true
 
   validates_presence_of :password, message: "密码不能为空",
@@ -15,9 +18,14 @@ class User < ApplicationRecord
   validates_length_of :password, message: "密码最短为6位", minimum: 6,
     if: :need_validate_password
 
-  private
-  def need_validate_password
-    self.new_record? ||
-      (!self.password.nil? || !self.password_confirmation.nil?)
+  def username
+    self.email.split('@').first
   end
+
+  private
+
+    def need_validate_password
+      self.new_record? ||
+        (!self.password.nil? || !self.password_confirmation.nil?)
+    end
 end
