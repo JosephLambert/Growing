ac13bdb2e536dad95745aad52f177c98ed0ad983
闫磊 <leiyan@fiberead.com>
2018-12-27 20:08:12 +0800

    rake wechat_menu

--- app/controllers/wechat/push_controller.rb
+++ app/controllers/wechat/push_controller.rb
@@ -1,10 +1,28 @@
 module Wechat
   class PushController < ApplicationController
 
+    # skip wechat post request
+    skip_before_action :verify_authenticity_token
+
+    before_action :auth_wechat_server
+
     def index
       Rails.logger.info "WECHAT: #{params}"
       render plain: params[:echostr]
     end
 
+    def data
+      body = request.body.read
+      Rails.logger.info "WECHAT: #{body}"
+
+      render plain: 'success'
+    end
+
+    protected
+    def auth_wechat_server
+      unless is_legal_from_wechat_server?
+        render plain: "illegal signature"
+      end
+    end
   end
 end
